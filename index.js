d4995b818158666a9521dc350ee0ed43a892c443
Dhiral Patel <dhiralpatel794@gmail.com>
2018-04-19 19:31:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,8 @@ function destructivelyRemoveLastKitten(name) {
   kittens.pop(name);
   return kittens;
 }
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens;
+}
