bcc189554b4d8b79972bd012f7a24e95beb3d4e4
Dhiral Patel <dhiralpatel794@gmail.com>
2018-04-19 19:28:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = "I don't do much."
 var kittens = ["Milo","Otis","Garfield"]
 
 function destructivelyAppendKitten(name) {
-  array.push();
-  return array
+  kittens.push(name);
+return kittens;
 }
