9c833141302b4947df52c5f387dd41bb36762d6d
Dhiral Patel <dhiralpatel794@gmail.com>
2018-04-19 19:49:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ function prependKitten(name) {
 }
 
 function removeLastKitten(name) {
-  var remkitten = kittens.slice(0,kittens.length - 1);;
+  var remkitten = kittens.slice(0,kittens.length - 1);
   return remkitten;
 }
