40060e978000af0549d262772fccc3a0c8123749
Dhiral Patel <dhiralpatel794@gmail.com>
2018-04-19 19:50:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,6 +37,6 @@ function removeLastKitten(name) {
 }
 
 function removeFirstKitten(name) {
-  var firkitten = kittens.slice(0,kittens.length - 1);
+  var firkitten = kittens.slice(4,kittens.length - 1);
   return firkitten;
 }
